13f99e134e310ead54f96ef9877234266a331419
Jeff Williams <jeff@autumnlanepaperie.com>
2022-02-18 13:35:34 -0800

    Update wp-custom-font-preview.php

--- wp-custom-font-preview.php
+++ wp-custom-font-preview.php
@@ -17,7 +17,7 @@
  * Plugin URI:        https://wpfontpreview.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.1
- * Author:            Jeffery Karbowski
+ * Author:            Jeff Williams
  * Author URI:        https://wpfontpreview.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
