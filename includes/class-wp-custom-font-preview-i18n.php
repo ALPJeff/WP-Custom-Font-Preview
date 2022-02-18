aad6e01a6a78db71d12e7f6d4b7d1d6cc4b14431
Jeff Williams <jeff@autumnlanepaperie.com>
2022-02-18 13:33:17 -0800

    Update class-wp-custom-font-preview-i18n.php

--- includes/class-wp-custom-font-preview-i18n.php
+++ includes/class-wp-custom-font-preview-i18n.php
@@ -6,7 +6,7 @@
  * Loads and defines the internationalization files for this plugin
  * so that it is ready for translation.
  *
- * @link       https://jefferykarbowski.com
+ * @link       https://wpfontpreview.com
  * @since      1.0.0
  *
  * @package    Wp_Custom_Font_Preview
@@ -22,7 +22,7 @@
  * @since      1.0.0
  * @package    Wp_Custom_Font_Preview
  * @subpackage Wp_Custom_Font_Preview/includes
- * @author     Jeffery Karbowski <jefferykarbowski@gmail.com>
+ * @author     Jeff Williams <jeff@autumnlanepaperie.com>
  */
 class Wp_Custom_Font_Preview_i18n {
 
