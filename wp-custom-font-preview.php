302b3276ff96365deccca0bd14f7ae5e58fc0288
Jeff Williams <jeff@autumnlanepaperie.com>
2022-02-21 11:00:00 -0800

    Update wp-custom-font-preview.php

--- wp-custom-font-preview.php
+++ wp-custom-font-preview.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       WP Custom Font Preview
  * Plugin URI:        https://wpfontpreview.com
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       WP Custom Font Preview allows WordPress websites to pair the plugin with Brainstorm Force's Custom Fonts plugin & allow website visitors to view their text in fonts defined by the website owner.
  * Version:           1.0.2
  * Author:            Jeff Williams
  * Author URI:        https://wpfontpreview.com
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WP_CUSTOM_FONT_PREVIEW_VERSION', '1.0.1' );
+define( 'WP_CUSTOM_FONT_PREVIEW_VERSION', '1.0.2' );
 
 /**
  * The code that runs during plugin activation.
